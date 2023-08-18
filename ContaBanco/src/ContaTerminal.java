34bb334e46b11bd41fcc6fa393d581e9b5601f37
andradean <109110750+andradean@users.noreply.github.com>
2023-08-18 12:35:27 -0300

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -7,7 +7,7 @@ public class ContaTerminal {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-        final String agencyBank = "067-8";
+        	final String agencyBank = "067-8";
 
 				
 		System.out.println("Por favor, digite o número da agência: ");
@@ -15,14 +15,14 @@ public class ContaTerminal {
 		sc.nextLine();		
 		System.out.println("Por favor insira o seu nome: ");
 		String name = sc.nextLine();
-        System.out.println("Por favor insira o seu saldo inicial: ");
-        double balance = sc.nextDouble();
+        	System.out.println("Por favor insira o seu saldo inicial: ");
+        	double balance = sc.nextDouble();
 		
         
 
 		System.out.println("Olá " + name + ", " + "obrigado por criar uma conta em nosso banco, sua agência é " + agencyBank + ", conta " + 
-        account + " e seu saldo " + balance + " já está disponivel para saque"
-        );
+        	account + " e seu saldo " + balance + " já está disponivel para saque"
+        	);
 		
 		sc.close();		
 		
